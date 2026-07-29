3e747adb9197f9aa912e919df9ffea95d537b47d
Jezreel Tan <jezdtanslc90@gmail.com>
2026-07-29 20:17:29 +0800

    mobile friendly fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen flex items-center justify-center bg-slate-50 p-4 md:p-20">
-        <div class="max-w-md w-full bg-white rounded-xl shadow-lg p-2 md:p-10 text-center">
+        <div class="max-w-md w-full bg-white rounded-xl shadow-lg px-2 py-8  md:p-10 text-center">
             <h1 class="text-3xl font-semibold mb-4 text-slate-900">Welcome to JW Library Notes Viewer</h1>
             <p class="text-slate-600 mb-8">Open your JW Library backup and browse personal notes in a clean, dedicated
                 space.</p>
